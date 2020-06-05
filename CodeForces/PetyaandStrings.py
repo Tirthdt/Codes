b1ae0990ea71e96ba376759820d2c3c3f8ff2870
Tirth Trivedi <tirtht2@gmail.com>
2020-06-05 12:53:34 +0530

    Small modifications

--- CodeForces/PetyaandStrings.py
+++ CodeForces/PetyaandStrings.py
@@ -1,3 +1,21 @@
+""" 
+To find which string is lexiographically bigger ignoring the cases.
+
+aaaa
+AAAA
+o/p: 0
+
+abs
+aBz
+o/p: -1
+
+abcdefg
+AbCdEfF
+o/p: 1
+
+
+"""
+
 string_1 = input()
 string_2 = input()
 if string_1.lower() < string_2.lower():
